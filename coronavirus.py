3db9438f4a7353e22d282903777b01b8bff5bf69
Nuno Ferro <nafferro@users.noreply.github.com>
2020-05-07 16:53:55 +0100

    Update coronavirus.py
    remove old stuff

--- coronavirus.py
+++ coronavirus.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options  
-from time import sleep
-import re
-from datetime import datetime
-import smtplib
 
 options = Options()  
 options.add_argument("--headless")  
